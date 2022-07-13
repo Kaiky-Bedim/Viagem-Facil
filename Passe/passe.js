ae412359967a9832c037339311576d70e5565941
Dias <jvdm123d@gmail.com>
2022-07-13 14:36:49 -0300

    Padronizando o passe

---
+++ Passe/passe.js
@@ -0,0 +1,8 @@
+import { Layout } from "../Layout/layout.js";
+
+var layout = new Layout();
+
+//Esses métodos carregar NavBar e Foot recebem o caminho do head ou foot a partir do html desse script
+//e o caminho para o CSS do Layout a partir daqui
+layout.carregarNavBar("../Layout/head.html", "../Layout/styleLayout.css");
+layout.carregarFoot("../Layout/foot.html", "../Layout/styleLayout.css");
