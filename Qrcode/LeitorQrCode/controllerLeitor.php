0509c52e6b4aa87cd8ba3bb81e3a42fbd828d313
Joao Vitor Dias <joaodias@appstorm.com.br>
2022-10-23 17:59:04 -0300

    Percurso Leitor

--- Qrcode/LeitorQrCode/controllerLeitor.php
+++ Qrcode/LeitorQrCode/controllerLeitor.php
@@ -16,7 +16,7 @@ $cpf = $_SESSION['cpf'];
 $data = json_decode(file_get_contents('php://input'), true);
 $numSerie = $data['numSerie'];
 $empresaCartao = $data['Empresa'];
-
+$idPercurso = $data['Id_Percurso'];
 //Convertendo os caracteres de Tabela para Caracteres Especiais novamente
 $empresaCartao = $formatador->FormatarTabelaParaCaracteresEspeciais($empresaCartao);
 
@@ -45,13 +45,12 @@ $resp = $leitor->DescontarPasse();
 
 if($resp == false){
     echo "Saldo Insuficiente!!!";
-    
 }else{
     echo "Passe Aprovado!!!";
 
-    $idPercurso = $leitor->GerarPercurso();
+    $id_Percurso = $leitor->PegarPercurso($idPercurso);
     #Falta a Movimentação
-    $leitor->GerarMovimentacao($idPercurso, $resp);
+    $leitor->GerarMovimentacao($id_Percurso, $resp);
 
 }
 
